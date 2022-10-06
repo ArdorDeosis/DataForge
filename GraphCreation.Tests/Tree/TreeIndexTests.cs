60978907010ed2a632e45ab263810f17d864b2f2
Theobald Beyer <theobald.beyer@t-online.de>
2022-10-06 16:42:01 +0200

    removed recursion from TreeIndex

--- GraphCreation.Tests/Tree/TreeIndexTests.cs
+++ GraphCreation.Tests/Tree/TreeIndexTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace GraphCreation.Tests;
 
@@ -8,27 +9,75 @@ public class TreeIndexTests
   public void TestDefaultValues()
   {
     // ARRANGE
-    var treeIndex = new TreeIndex();
+    var index = new TreeIndex();
 
     // ASSERT
-    Assert.That(treeIndex.ChildIndex, Is.Zero);
-    Assert.That(treeIndex.ParentIndex, Is.Null);
+    Assert.That(index.ChildIndex, Is.Zero);
+    Assert.That(index.ParentIndex, Is.Null);
   }
 
   [Test]
   public void Initializer_InvalidChildIndex_ThrowsArgumentException() =>
     Assert.That(() => new TreeIndex(null!, -1), Throws.ArgumentException);
 
-  [Test]
-  public void Equals_SameValues_True()
-  {
-    // ARRANGE
-    var root1 = new TreeIndex();
-    var root2 = new TreeIndex();
-    var child1 = new TreeIndex(parentIndex: root1, childIndex: 0);
-    var child2 = new TreeIndex(parentIndex: root2, childIndex: 0);
 
+  [TestCaseSource(nameof(TreeIndicesAndExpectedDepths))]
+  public void Depth_ExpectedValue(TreeIndex index, int expectedDepth) =>
+    Assert.That(index.Depth, Is.EqualTo(expectedDepth));
+
+  [TestCaseSource(nameof(EqualPairs))]
+  public void Equals_ValueEquivalentPairs_True(TreeIndex a, TreeIndex b)
+  {
     // ASSERT
-    Assert.That(child1, Is.EqualTo(child2));
+    Assert.That(a.Equals(b), Is.True);
+    Assert.That(a.Equals((object)b), Is.True);
+  }
+
+  [Test]
+  public void Equals_NullTreeIndex_False()
+  {
+    // ASSERT
+    Assert.That(new TreeIndex().Equals(null), Is.False);
+  }
+
+  [TestCaseSource(nameof(UnequalPairs))]
+  public void Equals_NonEquivalentPairs_False(TreeIndex index, object? other)
+  {
+    // ASSERT
+    Assert.That(index.Equals(other), Is.False);
+  }
+
+  [TestCaseSource(nameof(EqualPairs))]
+  public void GetHashCode_SameValues_True(TreeIndex a, TreeIndex b) =>
+    Assert.That(a.GetHashCode(), Is.EqualTo(b.GetHashCode()));
+
+  private static IEnumerable<object[]> EqualPairs()
+  {
+    var instance = new TreeIndex();
+    yield return new object[] { instance, instance };
+    yield return new object[] { new TreeIndex(), new TreeIndex() };
+    yield return new object[] { TreeIndexHelper.FromArray(0), TreeIndexHelper.FromArray(0) };
+    yield return new object[]
+    {
+      TreeIndexHelper.FromArray(0xC0FFEE, 0xBEEF, 0xF00D),
+      TreeIndexHelper.FromArray(0xC0FFEE, 0xBEEF, 0xF00D),
+    };
+  }
+
+  private static IEnumerable<object?[]> UnequalPairs()
+  {
+    yield return new object?[] { new TreeIndex(), null };
+    yield return new object?[] { new TreeIndex(), new { } };
+    yield return new object[] { TreeIndexHelper.FromArray(0), TreeIndexHelper.FromArray(1) };
+    yield return new object[] { TreeIndexHelper.FromArray(0), TreeIndexHelper.FromArray(0, 0) };
+  }
+
+  private static IEnumerable<object[]> TreeIndicesAndExpectedDepths()
+  {
+    yield return new object[] { new TreeIndex(), 0 };
+    yield return new object[] { TreeIndexHelper.FromArray(0), 1 };
+    yield return new object[] { TreeIndexHelper.FromArray(0, 0, 0), 3 };
+    yield return new object[] { TreeIndexHelper.FromArray(0xC0FFEE, 0xBEEF, 0xF00D), 3 };
+    yield return new object[] { TreeIndexHelper.FromArray(1, 2, 3, 4, 5, 6, 7, 8), 8 };
   }
 }
--- GraphCreation/Tree/TreeIndex.cs
+++ GraphCreation/Tree/TreeIndex.cs
@@ -13,42 +13,54 @@ public class TreeIndex : IEquatable<TreeIndex>
   {
     ParentIndex = parentIndex;
     ChildIndex = Guard.Against.Negative(childIndex);
+    Depth = parentIndex.Depth + 1;
   }
 
   public TreeIndex? ParentIndex { get; private init; }
 
   public int ChildIndex { get; private init; }
 
-  public int Depth => ParentIndex is { Depth: var parentDepth } ? parentDepth + 1 : 0;
+  public int Depth { get; private init; }
+
   public bool IsRoot => ParentIndex is null;
 
 
   /// <inheritdoc />
   public bool Equals(TreeIndex? other)
   {
-    if (ReferenceEquals(null, other))
-      return false;
-    if (ReferenceEquals(this, other))
-      return true;
-    return Equals(ParentIndex, other.ParentIndex) && ChildIndex == other.ChildIndex;
+    if (other is null) return false;
+    if (ReferenceEquals(this, other)) return true;
+
+    var a = this;
+    var b = other;
+    while (true)
+    {
+      if (a.ChildIndex != b.ChildIndex) return false;
+      a = a.ParentIndex;
+      b = b.ParentIndex;
+      if (a is null && b is null) return true;
+      if (a is null || b is null) return false;
+    }
   }
 
   /// <inheritdoc />
   public override bool Equals(object? obj)
   {
-    if (ReferenceEquals(null, obj))
-      return false;
-    if (ReferenceEquals(this, obj))
-      return true;
-    return obj.GetType() == GetType() && Equals((TreeIndex)obj);
+    if (obj is null) return false;
+    if (ReferenceEquals(this, obj)) return true;
+    return obj is TreeIndex treeIndex && Equals(treeIndex);
   }
 
   /// <inheritdoc />
-  public override int GetHashCode() =>
-    ParentIndex is null
-      ? ChildIndex
-      : HashCode.Combine(ParentIndex.GetHashCode(), ChildIndex);
+  public override int GetHashCode()
+  {
+    if (IsRoot) return 0xC0FFEE; // don't use 0; the first child added to root will have 0
+    var currentHash = ChildIndex;
+    for (var index = this; index.ParentIndex is not null; index = index.ParentIndex)
+      currentHash = HashCode.Combine(currentHash, index.ParentIndex.ChildIndex);
+    return currentHash;
+  }
 
   /// <inheritdoc />
-  public override string ToString() => IsRoot ? "root" : $"{ParentIndex}-{ChildIndex}";
+  public override string ToString() => IsRoot ? "root" : $"{ParentIndex}->{ChildIndex}";
 }
