3be711f0a8f88d7daaad391fd1b3abbbe64f7859
Theobald Beyer <theobald.beyer@t-online.de>
2022-10-08 20:13:42 +0200

    added ring graph creation tests

---
+++ GraphCreation.Tests/Ring/RingGraphCreationOptionTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+namespace GraphCreation.Tests;
+
+public class RingGraphCreationOptionTests
+{
+  [TestCase(0)]
+  [TestCase(-1)]
+  public void Initializer_InvalidSize_ThrowsArgumentException(int invalidLength) =>
+    Assert.That(() => new RingGraphCreationOption<int, int> { Size = invalidLength }, Throws.ArgumentException);
+}
