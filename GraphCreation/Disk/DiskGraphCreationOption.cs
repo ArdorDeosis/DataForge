c2c807cbfb553096fa0b494ad6069489feb7be41
Theobald Beyer <theobald.beyer@t-online.de>
2022-10-08 19:23:09 +0200

    added disk graph creation

---
+++ GraphCreation/Disk/DiskGraphCreationOption.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.GuardClauses;
+using GraphCreation.Disk;
+using JetBrains.Annotations;
+
+namespace GraphCreation;
+
+[PublicAPI]
+// TODO: use field keyword
+public sealed class DiskGraphCreationOption<TNodeData, TEdgeData>
+  : IndexedGraphDataCreationOption<DiskIndex, TNodeData, TEdgeData>
+{
+  private readonly int meridianCount;
+  private readonly int ringCount;
+
+  public /*required*/ int MeridianCount
+  {
+    get => meridianCount;
+    init => meridianCount = Guard.Against.NegativeOrZero(value, nameof(MeridianCount));
+  }
+
+  public /*required*/ int RingCount
+  {
+    get => ringCount;
+    init => ringCount = Guard.Against.Negative(value, nameof(RingCount));
+  }
+
+  public EdgeDirection MeridianEdgeDirection { get; init; } = EdgeDirection.Forward;
+  public EdgeDirection RingEdgeDirection { get; init; } = EdgeDirection.Forward;
+
+  public bool MakeCenterNode { get; init; } = true;
+}
