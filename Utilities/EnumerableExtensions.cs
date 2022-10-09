d2f63de80f70ab28f0792af7182544ae92e92b17
Theobald Beyer <theobald.beyer@t-online.de>
2022-10-09 19:17:04 +0200

    removed unused utility extension

--- Utilities/EnumerableExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Utilities;
-
-public static class EnumerableExtensions
-{
-  public static IEnumerable<(T1, T2)> PairWith<T1, T2>(this IEnumerable<T1> collection1, IEnumerable<T2> collection2) =>
-    from item1 in collection1 from item2 in collection2 select (item1, item2);
-}
