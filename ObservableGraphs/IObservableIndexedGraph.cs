d460ebb85260e96d851be3ac006996484fa85d4f
Theobald Beyer <theobald.beyer@t-online.de>
2023-01-28 22:26:17 +0100

    adjusted structure of indexed observable graphs

--- ObservableGraphs/IObservableIndexedGraph.cs
+++ ObservableGraphs/IObservableIndexedGraph.cs
@@ -4,8 +4,5 @@ namespace ObservableGraphs;
 
 public interface IObservableIndexedGraph<TIndex, TNodeData, TEdgeData> : IIndexedGraph<TIndex, TNodeData, TEdgeData> where TIndex : notnull
 {
-  event EventHandler<IndexedNodeAddedEventArgs<TIndex, TNodeData, TEdgeData>> IndexedNodeAdded;
-  event EventHandler<IndexedNodeRemovedEventArgs<TIndex, TNodeData, TEdgeData>> IndexedNodeRemoved;
-  event EventHandler<IndexedEdgeAddedEventArgs<TIndex, TNodeData, TEdgeData>> IndexedEdgeAdded;
-  event EventHandler<IndexedEdgeRemovedEventArgs<TIndex, TNodeData, TEdgeData>> IndexedEdgeRemoved;
+  event EventHandler<IndexedGraphChangedEventArgs<TIndex, TNodeData, TEdgeData>> GraphChanged;
 }
---
+++ ObservableGraphs/IndexedGraphChangedEventArgs.cs
@@ -0,0 +1,62 @@
+﻿using DataForge.Graphs;
+using JetBrains.Annotations;
+
+namespace ObservableGraphs;
+
+[PublicAPI]
+public sealed class IndexedGraphChangedEventArgs<TIndex, TNodeData, TEdgeData> : EventArgs where TIndex : notnull
+{
+  internal IndexedGraphChangedEventArgs(
+    IReadOnlyCollection<IndexedNode<TIndex, TNodeData, TEdgeData>> addedNodes,
+    IReadOnlyCollection<IndexedNode<TIndex, TNodeData, TEdgeData>> removedNodes,
+    IReadOnlyCollection<IndexedEdge<TIndex, TNodeData, TEdgeData>> addedEdges,
+    IReadOnlyCollection<IndexedEdge<TIndex, TNodeData, TEdgeData>> removedEdges
+  )
+  {
+    AddedNodes = addedNodes;
+    RemovedNodes = removedNodes;
+    AddedEdges = addedEdges;
+    RemovedEdges = removedEdges;
+  }
+
+  public IReadOnlyCollection<IndexedNode<TIndex, TNodeData, TEdgeData>> AddedNodes { get; }
+  public IReadOnlyCollection<IndexedNode<TIndex, TNodeData, TEdgeData>> RemovedNodes { get; }
+  public IReadOnlyCollection<IndexedEdge<TIndex, TNodeData, TEdgeData>> AddedEdges { get; }
+  public IReadOnlyCollection<IndexedEdge<TIndex, TNodeData, TEdgeData>> RemovedEdges { get; }
+
+  internal static IndexedGraphChangedEventArgs<TIndex, TNodeData, TEdgeData> NodesAdded(
+    params IndexedNode<TIndex, TNodeData, TEdgeData>[] nodes) =>
+    new(
+      nodes,
+      Array.Empty<IndexedNode<TIndex, TNodeData, TEdgeData>>(),
+      Array.Empty<IndexedEdge<TIndex, TNodeData, TEdgeData>>(),
+      Array.Empty<IndexedEdge<TIndex, TNodeData, TEdgeData>>()
+    );
+
+  internal static IndexedGraphChangedEventArgs<TIndex, TNodeData, TEdgeData> NodesRemoved(
+    params IndexedNode<TIndex, TNodeData, TEdgeData>[] nodes) =>
+    new(
+      Array.Empty<IndexedNode<TIndex, TNodeData, TEdgeData>>(),
+      nodes,
+      Array.Empty<IndexedEdge<TIndex, TNodeData, TEdgeData>>(),
+      Array.Empty<IndexedEdge<TIndex, TNodeData, TEdgeData>>()
+    );
+
+  internal static IndexedGraphChangedEventArgs<TIndex, TNodeData, TEdgeData> EdgesAdded(
+    params IndexedEdge<TIndex, TNodeData, TEdgeData>[] edges) =>
+    new(
+      Array.Empty<IndexedNode<TIndex, TNodeData, TEdgeData>>(),
+      Array.Empty<IndexedNode<TIndex, TNodeData, TEdgeData>>(),
+      edges,
+      Array.Empty<IndexedEdge<TIndex, TNodeData, TEdgeData>>()
+    );
+
+  internal static IndexedGraphChangedEventArgs<TIndex, TNodeData, TEdgeData> EdgesRemoved(
+    params IndexedEdge<TIndex, TNodeData, TEdgeData>[] edges) =>
+    new(
+      Array.Empty<IndexedNode<TIndex, TNodeData, TEdgeData>>(),
+      Array.Empty<IndexedNode<TIndex, TNodeData, TEdgeData>>(),
+      Array.Empty<IndexedEdge<TIndex, TNodeData, TEdgeData>>(),
+      edges
+    );
+}
